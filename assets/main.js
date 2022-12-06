a7f800b7d8583715a915030facf938255346065d
rizal <muhammadrizal2252@gmail.com>
2022-12-06 17:02:28 +0700

    Create HTML Template

--- assets/main.js
+++ assets/main.js
@@ -16,14 +16,11 @@ function onSubmitBtnClicked(){
         body: JSON.stringify(reqBody)
     }).
     then(res => res.json()).
-    then(json => {
-        console.log(json)
-        var res = JSON.parse(json)
-
-        if(res.status_code != 201){
+    then(json => {      
+        if(json.status_code != 201){
             urlTextfield.value = "URL isn't valid"
         } else {
-            urlTextfield.value = "https://shrt.go/"+res.data.short_url
+            urlTextfield.value = "https://shrt.go/"+json.data.short_url
         }
     })
 }
