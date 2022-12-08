26c6a63aa7fa4ab0837ee1603243fd3c464cba85
rizal <muhammadrizal2252@gmail.com>
2022-12-08 16:33:41 +0700

    Add user_id and cookie

--- db/queries/queries.go
+++ db/queries/queries.go
@@ -4,13 +4,13 @@ package queries
 const InsertURL string = `INSERT INTO urls (url, short_url, created_at, user_id) VALUES (?,?,?,?)`
 
 // Find URL by Short URL
-const FindByShort string = `SELECT id, url, short_url, click_count, created_at FROM urls WHERE short_url = ?`
+const FindByShort string = `SELECT id, url, short_url, click_count, created_at, user_id FROM urls WHERE short_url = ?`
 
 // Find URL by URL ID
-const FindByID string = `SELECT id, url, short_url, click_count, created_at FROM urls WHERE id = ?`
+const FindByID string = `SELECT id, url, short_url, click_count, created_at, user_id FROM urls WHERE id = ?`
 
 // Find All Url
-const FindAll string = `SELECT id, url, short_url, click_count, created_at FROM urls`
+const FindAll string = `SELECT id, url, short_url, click_count, created_at, user_id FROM urls`
 
 // Delete URL by ID
 const DeleteByID = `DELETE FROM urls WHERE id = ?`
